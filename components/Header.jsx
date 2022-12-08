0310083b2b129063fc6f1e7a85522d2e3d9d5d77
innocentdavid <paulinnocent04@gmail.com>
2022-12-08 16:19:51 +0100

    udp

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,15 +4,36 @@ import React, { useState } from 'react'
 
 export default function Header() {
     const [showRegModal, setShowRegModal] = useState(false)
+    const [showMobileMenu, setShowMobileMenu] = useState(false)
 
-    return (
+    return (<>
+        {showMobileMenu && <div className="">
+            <div className="fixed top-0 left-0 w-full h-screen bg-black/50"></div>
+            <div className="fixed top-0 right-0 w-[266px] h-screen bg-primary-900 text-white pl-[15px]">
+                <div className="flex justify-end my-[22px] mr-[25px]">
+                    <Image src="/images/Close.svg" alt="" width={24} height={24} onClick={() => setShowMobileMenu(false)} />
+                </div>
+                <div className="flex flex-col gap-[15px]">
+                    <Link href="#Become_a_provider" className="text-[13px] font-[400]">Become a provider</Link>
+                    <Link href="#Hire_a_worker" className="text-[13px] font-[400]">Hire a worker</Link>
+                    <Link href="#Contact_us" className="text-[13px] font-[400]">Contact us</Link>
+                </div>
+                <div className="flex flex-col gap-[25px] mt-[40px]">
+                    <Link href="/login" className="w-[191px] h-[50px] grid place-items-center border border-white bg-white text-primary-900 rounded-[10px]">Login</Link>
+                    <Link href="/signup" className="w-[191px] h-[50px] grid place-items-center border border-white bg-primary-900 text-white rounded-[10px]">Register</Link>
+                </div>
+                <Link href="/signup/work" className="flex items-center gap-3 mt-[22px]">Sign up to work
+                    <Image src="/images/Arrow right alt white.png" alt='' width={20} height={20} className="mt-1" />
+                </Link>
+            </div>
+        </div>}
         <header className="px-[24px] md:px-[49px]">
             <nav className="flex justify-between items-center pt-5 md:pr-20">
                 <Image src="/images/logo 1.png" alt="" width={120} height={55} />
 
                 <div className="flex gap-5 lg:gap-10 items-center">
                     <Image src="/images/Search.svg" alt="" width={34} height={34} />
-                    <Image src="/images/Menu.svg" alt="" width={34} height={34} className="lg:hidden" />
+                    <Image src="/images/Menu.svg" alt="" width={34} height={34} className="lg:hidden" onClick={() => setShowMobileMenu(true) } />
 
                     <div className="hidden lg:flex gap-10 items-center">
                         <div className='cursor-pointer text-2xl font-[400]' onClick={() => setShowRegModal(!showRegModal)}>Register</div>
@@ -35,5 +56,5 @@ export default function Header() {
                 </div>
             </div>}
         </header>
-    )
+    </>)
 }
